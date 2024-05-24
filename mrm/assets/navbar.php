9e356f7361f8c37f31c1834b4e3a11ebb7dbfe55
Caique Moreira <47729557+MrMoreira@users.noreply.github.com>
2024-05-24 17:51:14 -0300

    att

---
+++ mrm/assets/navbar.php
@@ -0,0 +1,16 @@
+<nav>
+       <div class="logo"><img src="img\MrAcademy.png" alt="MrMoreira Academy"></div>
+       <div class="menu">
+              <ul>
+                     <li><a href="#">Home</a></li>
+                     <li><a href="#">Ranking</a></li>
+                     <li><a href="#">Others</a></li>
+              </ul>
+       </div>
+       <div class="navlogin">
+              <ul>
+                     <li><a href="#">Login</a></li>
+                     <li><a href="#">Registrar</a></li>
+              </ul>
+       </div>
+</nav>
---
+++ mrm/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/colors.css">
+    <link rel="stylesheet" href="./assets/css/navbar.css">
+    <title>MrMoreira Academy</title>
+</head>
+<body>
+    <?php include './assets/navbar.php' ?>
+</body>
+</html>
